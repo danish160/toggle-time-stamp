4c574014b2af77f08b05d089533fa63cf126be84
danish160 <danish160@yahoo.com>
2019-10-28 16:10:31 +1100

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ToggleTimeStampComponent } from './toggle-time-stamp/toggle-time-stamp.component';
-import { TimeagoPipe } from './timeago.pipe';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { ToggleTimeStampComponent } from "./toggle-time-stamp/toggle-time-stamp.component";
+import { TimeagoPipe } from "./timeago.pipe";
+import { CookieService } from "ngx-cookie-service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ToggleTimeStampComponent, TimeagoPipe ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ToggleTimeStampComponent,
+    TimeagoPipe
+  ],
+  providers: [CookieService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/toggle-time-stamp/toggle-time-stamp.component.ts
+++ src/app/toggle-time-stamp/toggle-time-stamp.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: "app-toggle-time-stamp",
   templateUrl: "./toggle-time-stamp.component.html",
   styleUrls: ["./toggle-time-stamp.component.css"]
 })
-export class ToggleTimeStampComponent {
+export class ToggleTimeStampComponent implements OnInit {
   @Input() timestamp: Date;
-   @Input() showTimeAgo: true;
-  constructor() {
+  // @Input()
+  showTimeAgo: boolean;
+  constructor(private cookieService: CookieService) {}
+  ngOnInit() {
+    const cookieExists: boolean = this.cookieService.check("showTimeAgo");
+    if (cookieExists) {
+      console.log("cookieExists");
+      const value: string = this.cookieService.get("showTimeAgo");
+      this.showTimeAgo = value.toLocaleLowerCase() == "true";
+    } else {
+      console.log("no cookieExists");
+      this.showTimeAgo = true;
+      this.cookieService.set("showTimeAgo", "true");
+    }
   }
 }
